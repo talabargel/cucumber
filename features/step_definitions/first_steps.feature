8bf8733645c43b6a5a480688f5613104f4e1551b
Tal <talab@edu.hac.ac.il>
2023-01-24 12:03:33 +0200

    cucumber

---
+++ features/step_definitions/first_steps.feature
@@ -0,0 +1,6 @@
+Feature: User is able to convert area units
+
+  Scenario: User is able to see from and to panels
+    Given I click on got is button
+    And I see sq Km in from header
+    And  I see sq Metre in to header
---
+++ features/step_definitions/first_steps.rb
@@ -0,0 +1,10 @@
+Given(/^I click on got is button$/) do
+  puts("Clicked got it button")
+end
+And(/^I see sq Km in from header$/) do
+  puts("Saw sq km in from header")
+end
+
+And(/^I see sq Metre in to header$/) do
+  puts("Saw sq mtr in to header")
+end
